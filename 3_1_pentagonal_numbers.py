acd9cd41306114d660cdecbb85b2a6d3d9b1d9df
Jay Adams <32779194+jayadams011@users.noreply.github.com>
2018-03-06 12:44:03 -0800

    added homework 3.1 nd 3.2

---
+++ 3_1_pentagonal_numbers.py
@@ -0,0 +1,10 @@
+# (Math: pentagonal numbers) A pentagonal number is defined as n(3n-1)/2 for and so on. So, the first
+# few numbers are 1, 5, 12, 22, .... Write a function with the following header that returns a
+# pentagonal number:
+
+def getPentagonalNumber(n):
+    for row in range(0,10):
+        for col in range(0,10):
+            pentagonalNumber = n(3n-1)/2
+            print pentgonalNumber 
+ 
---
+++ 3_2_palindrome_int.py
@@ -0,0 +1,18 @@
+is_it_palidrome = eval(input("Enter a number to see if it is a palindrome. --> "))
+
+def reverse(number):
+    s=len(number)
+    newnum=[None]*length
+    for i in num:
+        s=s-1
+        newnum[s]=i
+        return newnum
+
+def palindrome(number):
+    a=str(number)
+    l=len(z)/2
+    if a[:1]==a[-1:][::-1]:
+        b=True
+    else:
+        b=False
+
